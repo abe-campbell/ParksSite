6955c6a95ac02428daa3f86094c8f4dda199b12f
abe-campbell <abecampbell77@gmail.com>
2024-12-22 23:34:26 -0500

    holy cannoli thats a lot of nonsense

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,20 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
 import 'leaflet/dist/leaflet.css';
 import parks from './components/parkinfo.json';
+import 'leaflet/dist/leaflet.css';
+
 
 // Dynamically import components with no server-side rendering
-const MapContainer = dynamic(
+const MapContainerNoSSR = dynamic(
   () => import('react-leaflet').then((mod) => mod.MapContainer),
   { ssr: false }
 );
 
-const TileLayer = dynamic(
+const TileLayerNoSSR = dynamic(
   () => import('react-leaflet').then((mod) => mod.TileLayer),
-  { ssr: false }
-);
-
-const useMap = dynamic(
-  () => import('react-leaflet').then((mod) => mod.useMap),
   { ssr: false }
 );
 
@@ -25,25 +22,10 @@ const L = dynamic(() => import('leaflet'), { ssr: false });
 const ParkInfo = dynamic(() => import('./components/parks'), { ssr: false });
 const ParkMarker = dynamic(() => import('./components/markers'), { ssr: false });
 
-// Handle Leaflet marker icons
-if (typeof window !== 'undefined') {
-  const { Icon } = require('leaflet');
-  const markerIcon2x = require('leaflet/dist/images/marker-icon-2x.png');
-  const markerIcon = require('leaflet/dist/images/marker-icon.png');
-  const markerShadow = require('leaflet/dist/images/marker-shadow.png');
 
-  delete Icon.Default.prototype._getIconUrl;
-  Icon.Default.mergeOptions({
-    iconUrl: markerIcon.src,
-    iconRetinaUrl: markerIcon2x.src,
-    shadowUrl: markerShadow.src,
-  });
-}
-
-const ParkMarkers = () => {
+const ParkMarkers = memo(() => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedPark, setSelectedPark] = useState(null);
-  const map = useMap();
 
   const handleMarkerClick = (park) => {
     setSelectedPark(park);
@@ -60,12 +42,12 @@ const ParkMarkers = () => {
       />
     </>
   );
-};
+});
 
-function Main() {
+const Main = memo(() => {
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <MapContainer
+      <MapContainerNoSSR
         center={[35.307, -80.735]}
         zoom={6.5}
         minZoom={6.5}
@@ -77,15 +59,16 @@ function Main() {
         maxBoundsViscosity={1.0}
         style={{ height: '100%', width: '100%' }}
       >
-        <TileLayer
+        <TileLayerNoSSR
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
         <ParkMarkers />
-      </MapContainer>
+      </MapContainerNoSSR>
     </div>
   );
-}
+});
+
 
 export default Main;
 
