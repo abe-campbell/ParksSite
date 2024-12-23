ad963a14479613bb7e2c49caceed4167644ac9c6
abe-campbell <abecampbell77@gmail.com>
2024-12-22 22:58:22 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,19 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import parks from './components/parkinfo.json';
+import dynamic from 'next/dynamic';
+
+const MapContainerWithNoSSR = dynamic(
+  () => import('react-leaflet').then((mod) => mod.MapContainer),
+  { ssr: false } // Disable server-side rendering
+);
+
+const TileLayerWithNoSSR = dynamic(
+  () => import('react-leaflet').then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+
+const ParkMarkersWithNoSSR = dynamic(() => import('./components/parks'), { ssr: false });
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -49,7 +62,7 @@ function Main() {
 
   return(
     <div style={{ height: '100vh', width: '100%' }}>
-    <MapContainer
+    <MapContainerWithNoSSR
         center={[35.307, -80.735]}
         zoom={6.5}
         minZoom={6.5}
@@ -61,12 +74,12 @@ function Main() {
         maxBoundsViscosity={1.0}
         style={{ height: '100%', width: '100%' }}
     >
-        <TileLayer
+        <TileLayerWithNoSSR
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
       <ParkMarkers/>
-      </MapContainer>
+      </MapContainerWithNoSSR>
   </div>
     );
 }
